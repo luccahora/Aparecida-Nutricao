1536d60d1f850afba6a3c83f04b12b8fd5d22177
Lucca Hora <luccahora@gmail.com>
2020-07-14 04:53:04 -0300

    Preenchendo tabela com o valor do form

--- aparecida-nutricao/js/principal.js
+++ aparecida-nutricao/js/principal.js
@@ -1,3 +1,5 @@
+var titulo = document.querySelector("h1")
+
 var pacientes = document.querySelectorAll(".paciente")
 
 for (let paciente of pacientes) {
@@ -33,9 +35,42 @@ for (let paciente of pacientes) {
     }
 }
 
+/* Adicionando paciente na tabela */
+const botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(){
+    event.preventDefault()
+    
+    const form = document.querySelector("#form-adiciona");
+    
+    /* Pegando valor do form */
+    const nome = form.nome.value;
+    const altura = form.altura.value;
+    const peso = form.peso.value;
+    const gordura = form.gordura.value;
+    
+    /* Criando tags html */
+    const pacienteTr = document.createElement("tr");
+
+    const nomeTd = document.createElement("td");
+    const pesoTd = document.createElement("td");
+    const alturaTd = document.createElement("td");
+    const gorduraTd = document.createElement("td");
+    const imcTd = document.createElement("td");
+
+    /* Passando valor do form p tag */
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    /* Add dados na tabela */
+    const tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 
 
-
-
-
-
+});
