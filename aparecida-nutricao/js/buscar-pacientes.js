21c25d195871e457d04cdb8c59085ac47b9f2fb8
Lucca Hora <luccahora@gmail.com>
2020-07-18 20:02:29 -0300

    Capturando erros de requisição

--- aparecida-nutricao/js/buscar-pacientes.js
+++ aparecida-nutricao/js/buscar-pacientes.js
@@ -1,19 +1,31 @@
 var botaoBuscar = document.querySelector("#buscar-pacientes");
 
-botaoBuscar.addEventListener("click", function(){
-    
+botaoBuscar.addEventListener("click", function () {
+
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
-    xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        /* json para array */
-        var pacientes = JSON.parse(resposta);
-        
-        pacientes.forEach(paciente => {
-            adicionaPacienteNaTabela(paciente)
-        });
+    xhr.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+           
+            var resposta = xhr.responseText;
+           
+            /* json para array */
+            var pacientes = JSON.parse(resposta);
+            
+            /* add pacientes na tabela */
+            pacientes.forEach(paciente => {
+                adicionaPacienteNaTabela(paciente)
+            });
+        } else {
+            erroAjax.classList.remove("invisivel")
+        }
+
+
     })
 
     xhr.send();
